1df0c3730b2430781238a156b6541af4ceb268ed
warmoverdrive <sweetsthebear@gmail.com>
2020-12-02 12:36:35 -0800

    new header

---
+++ headers/Inputv3.h
@@ -0,0 +1,35 @@
+#ifndef INPUT1_H
+#define INPUT1_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <utility>
+
+template <typename T>
+struct Input
+{
+    std::vector<T> data;
+    Input(const std::string &path, char delim = ' ')
+    {
+        try
+        {
+            std::ifstream infile(path);
+            std::string line;
+            while (std::getline(infile, line, delim))
+            {
+                T element;
+                std::stringstream input(line);
+                while (input >> element)
+                    data.push_back(element);
+            }
+        }
+        catch (...)
+        {
+            std::cout << "An error occured!\n";
+        }
+    }
+};
+
+#endif
